ae7c85ffb919792ae60db0eaf7b9315f50ec227c
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-05-04 14:40:44 +0200

    input area

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App(): React.ReactElement {
   return (
     <>
-      <h3>Hello Vite!</h3>
+      <div style={{ margin: "10px" }}>
+        <textarea>Code Here</textarea>
+        <div>
+          <button style={{ background: "black", color: "white" }}>
+            Submit
+          </button>
+        </div>
+        <pre></pre>
+      </div>
     </>
   );
 }
