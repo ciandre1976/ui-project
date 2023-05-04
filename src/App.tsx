992a41f95810ec0bffa53f6800a79e15d4a6efa5
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-05-04 15:24:12 +0200

    wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect } from "react";
+
 function App(): React.ReactElement {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = () => {
+    console.log(input);
+  };
   return (
     <>
       <div style={{ margin: "10px" }}>
-        <textarea>Code Here</textarea>
+        <textarea onChange={(e) => setInput(e.target.value)} />
         <div>
-          <button style={{ background: "black", color: "white" }}>
+          <button
+            style={{ background: "black", color: "white" }}
+            onClick={onClick}
+          >
             Submit
           </button>
         </div>
-        <pre></pre>
+        <pre>{code}</pre>
       </div>
     </>
   );
